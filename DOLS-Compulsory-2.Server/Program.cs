dae239986999e2a90ff966997d84312f6eefff3f
Andreas Eriksen <pjuske.10@live.dk>
2025-04-08 12:31:02 +0200

    yes sir

--- DOLS-Compulsory-2.Server/Program.cs
+++ DOLS-Compulsory-2.Server/Program.cs
@@ -1,23 +1,23 @@
+using dols_compulsory_2.Server.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();           
+
+builder.Services.AddSingleton<NoteService>(); 
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.MapStaticAssets();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();                         
+app.UseSwaggerUI();                       
+
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
